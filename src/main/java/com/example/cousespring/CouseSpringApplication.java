884f6540f09a69eb3742d91a41632aa24138bfb2
Elson de Souza Santos1 <elson.de.souza.santos1@ibm.com>
2021-09-21 10:27:23 -0300

    iniciando spring

---
+++ src/main/java/com/example/cousespring/CouseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.cousespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouseSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouseSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cousespring/CouseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cousespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
